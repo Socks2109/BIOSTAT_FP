c6a3b4e7559c61384b21640740fac18403fb07c8
Isaac Yeoh <isaacyeohcj@gmail.com>
2024-11-28 13:57:32 -0800

    Made data file changes

---
+++ Isaac_Individual.R
@@ -0,0 +1,8 @@
+HIV_TB <- read.csv("data/HIV_TB_INCIDENCE_PREVALENCE_DEATHS.csv")
+Malaria <- read.csv("data/MALARIA_INCIDENCE_DEATHS.csv")
+
+library(dplyr)
+library(ggplot2)
+library(sqldf)
+library(tidyr)
+
