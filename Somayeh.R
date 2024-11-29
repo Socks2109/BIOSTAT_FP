76c62a01c4002d4926dda4703b27bae2378e4360
Isaac Yeoh <isaacyeohcj@gmail.com>
2024-11-29 12:00:26 -0800

    updated somayeh's portion

--- Somayeh.R
+++ Somayeh.R
@@ -9,16 +9,17 @@ library(infer)
 library(countrycode)
 library(gt)
 
-# Individual Portion
+# Research question: Is the mortality rate of HIV, TB, and Malaria
+# between 2002 and 2013 significantly more in Somalia than in Vietnam?
 
-# Somayeh
+# Null Hypothesis (H₀): The mortality rate of HIV, TB, and Malaria
+# between 2002 and 2013 in Somalia is not significantly more than in Vietnam.
 
-# Research question: Does the mortalities caused by HIV, TB, and malaria in Vietnam, and Somalia differ in the years 2002 and 2013?
-# Null Hypothesis (H₀): HIV, TB, and Malaria mortalities have not decreased in Vietnam, and Somalia significantly between the years 2002 and 2013.
+# Alternative Hypothesis (H₁): The cumulative mortality rate of HIV, TB,
+# and Malaria between 2002 and 2013 in Somalia is significantly more than in Vietnam.
 
-# Alternative Hypothesis (H₁): HIV, TB, and Malaria deaths have decreased in Vietnam, and Somalaia significantly between the years 2002 and 2013.
+# ==============================================================================
 
-# datasets are loaded above.
 # Using the countrycode package, retrieve every country's ISO3 code, e.g: Australia = AUS
 valid_iso3 <- unique(countrycode::codelist$iso3c)
 
@@ -31,182 +32,162 @@ HIV_TB_filtered <- HIV_TB %>%
   filter(sex_name == "Both sexes") %>%
   filter(year >= 2002)
 
+Malaria_filtered <- Malaria %>%
+  filter(age_group_name == "All Ages") %>%
+  filter(location_code %in% valid_iso3) %>%
+  filter(metric == "Deaths") %>%
+  filter(unit == "Number") %>%
+  filter(sex_name == "Both sexes") %>%
+  filter(year >= 2002)
 
 # HIV means for Somalia between the years 2002 and 2013
-
 HIV_data_Somalia <- HIV_TB_filtered %>%
   filter(year >= 2002 & year <= 2013) %>%
   filter(location_name == "Somalia") %>%
   filter(cause_name == "HIV/AIDS") %>%
-  select(year, mean)
-early_period <- filter(HIV_data_Somalia, year <= 2007)
-late_period <- filter(HIV_data_Somalia, year > 2007)
-early_means <- as.numeric(early_period$mean)
-late_means <- as.numeric(late_period$mean)
-print(HIV_data_Somalia)
-                      
-# Perform an independent t-test
-HIV_t_test <- t.test(early_means, late_means, alternative = "two.sided", conf.level = 0.95)
-print(HIV_t_test)
+  pull("mean")
 
-# Results: Two Sample t-test, t-value = -4.0391, p-value = 0.008055
-# alternative hypothesis: true mean difference is not equal to 0. Since the p-value < 0.05, we reject null hypothesis. Therefore, HIV deaths have decreased in Somalia between the years 2002 and 2013.
+HIV_data_Somalia <- as.numeric(HIV_data_Somalia)
 
-
-
-# TB means for Somalia in the years 2002 and 2013
-TB_data_Somalia <- HIV_TB_filtered %>%
-  filter(year >= 2002 & year <= 2013) %>%
-  filter(location_name == "Somalia") %>%
-  filter(cause_name == "Tuberculosis") %>%
-  select(year, mean)
-early_period <- filter(TB_data_Somalia, year <= 2007)
-late_period <- filter(TB_data_Somalia, year > 2007)
-early_means <- as.numeric(early_period$mean)
-late_means <- as.numeric(late_period$mean)
-print(TB_data_Somalia)
-
-# Perform an independent t-test
-TB_t_test <- t.test(early_means, late_means, alternative = "two.sided", conf.level = 0.95)
-print(TB_t_test)
-
-# Results: Two Sample t-test, t-value = -5.265, p-value = 0.0004222
-# alternative hypothesis: true mean difference is not equal to 0. Since the p-value < 0.05, we reject null hypothesis. Therefore, TB deaths have decreased in Somalia between the years 2002 and 2013.
-
-
-# Malaria means for Somalia in years 2002 and 2013
-Malaria_data_Somalia <- Malaria_filtered %>%
-  filter(year >= 2002 & year <= 2013) %>%
-  filter(location_name == "Somalia") %>%
-  filter(cause_name == "Malaria") %>%
-  select(year, mean)
-early_period <- filter(Malaria_data_Somalia, year <= 2007)
-late_period <- filter(Malaria_data_Somalia, year > 2007)
-early_means <- as.numeric(early_period$mean)
-late_means <- as.numeric(late_period$mean)
-print(Malaria_data_Somalia)
-
-# Perform an independent t-test
-Malaria_t_test <- t.test(early_means, late_means, alternative = "two.sided", conf.level = 0.95)
-print(Malaria_t_test)
-
-
-# Results: Two Sample t-test, t-value = 5.4488, p-value = 0.002031
-# alternative hypothesis: true mean difference is not equal to 0. Since the p-value < 0.05, we reject null hypothesis. Therefore, Malaria deaths have decreased in Somalia between the years 2002 and 2013.
-
-# Visualization
-HIV_data_Somalia$mean <- as.numeric(HIV_data_Somalia$mean)
-TB_data_Somalia$mean <- as.numeric(TB_data_Somalia$mean)
-
-HIV_data_Somalia_sum <- sum(HIV_data_Somalia, na.rm = TRUE)
-TB_data_Somalia_sum <- sum(TB_data_Somalia, na.rm = TRUE)
-Malaria_data_Somalia_sum <- sum(Malaria_data_Somalia, na.rm = TRUE)
-
-summary_data <- data.frame(
-  condition = rep(c("HIV", "Malaria", "TB"), each = 2),
-  year = rep(c(2002, 2013), times = 3),
-  sum_value = c(HIV_data_Somalia_sum, TB_data_Somalia_sum, Malaria_data_Somalia_sum)
-)
-
-print(summary_data)
-
-ggplot(summary_data, aes(x = condition, y = sum_value, fill = factor(year))) +
-  geom_bar(stat = "identity", position = "dodge") +
-  scale_fill_manual(values = c("2002" = "cornflowerblue", "2013" = "darkslategray1")) +
-  labs(
-    title = "Sum of Deaths for HIV, Malaria, and TB (2002 vs. 2013)",
-    x = "Disease",
-    y = "Sum of Deaths",
-    fill = "Year"
-  ) +
-  theme_minimal()
-
-
-
-
-# HIV means for Vietnam in years 2002 and 2013
 HIV_data_Vietnam <- HIV_TB_filtered %>%
   filter(year >= 2002 & year <= 2013) %>%
   filter(location_name == "Vietnam") %>%
   filter(cause_name == "HIV/AIDS") %>%
-  select(year, mean)
-early_period <- filter(HIV_data_Vietnam, year <= 2007)
-late_period <- filter(HIV_data_Vietnam, year > 2007)
-early_means <- as.numeric(early_period$mean)
-late_means <- as.numeric(late_period$mean)
-print(HIV_data_Vietnam)
+  pull("mean")
 
-# Perform an independent t-test
-HIV_t_test <- t.test(early_means, late_means, alternative = "two.sided", conf.level = 0.95)
-print(HIV_t_test)
+HIV_data_Vietnam <- as.numeric(HIV_data_Vietnam)
 
-# Results: Two Sample t-test, t-value = -1.8786, p-value = 0.1177
-# alternative hypothesis: true mean difference is not equal to 0. Since the p-value > 0.05, we fail to reject null hypothesis. Therefore, HIV deaths have not decreased in Vietnam between the years 2002 and 2013.
+TB_data_Somalia <- HIV_TB_filtered %>%
+  filter(year >= 2002 & year <= 2013) %>%
+  filter(location_name == "Somalia") %>%
+  filter(cause_name == "Tuberculosis") %>%
+  pull("mean")
 
+TB_data_Somalia <- as.numeric(TB_data_Somalia)
 
-# TB means for Vietnam in years 2002 and 2013
 TB_data_Vietnam <- HIV_TB_filtered %>%
   filter(year >= 2002 & year <= 2013) %>%
   filter(location_name == "Vietnam") %>%
   filter(cause_name == "Tuberculosis") %>%
-  select(year, mean)
-early_period <- filter(TB_data_Vietnam, year <= 2007)
-late_period <- filter(TB_data_Vietnam, year > 2007)
-early_means <- as.numeric(early_period$mean)
-late_means <- as.numeric(late_period$mean)
-print(TB_data_Vietnam)
+  pull("mean")
 
-# Perform an independent t-test
-TB_t_test <- t.test(early_means, late_means, alternative = "two.sided", conf.level = 0.95)
-print(TB_t_test)
+TB_data_Vietnam <- as.numeric(TB_data_Vietnam)
 
-# Results: Two Sample t-test, t-value = 4.1684, p-value = 0.008347
-# alternative hypothesis: true mean difference is not equal to 0. Since the p-value < 0.05, we reject null hypothesis. Therefore, TB deaths have decreased in Vietnam between the years 2002 and 2013.
+Malaria_data_Somalia <- Malaria_filtered %>%
+  filter(year >= 2002 & year <= 2013) %>%
+  filter(location_name == "Somalia") %>%
+  filter(cause_name == "Malaria") %>%
+  pull("mean")
 
+Malaria_data_Somalia <- as.numeric(Malaria_data_Somalia)
 
-
-# Malaria means for Vietnam in years 2002 and 2013
 Malaria_data_Vietnam <- Malaria_filtered %>%
   filter(year >= 2002 & year <= 2013) %>%
   filter(location_name == "Vietnam") %>%
   filter(cause_name == "Malaria") %>%
-  select(year, mean)
-early_period <- filter(Malaria_data_Vietnam, year <= 2007)
-late_period <- filter(Malaria_data_Vietnam, year > 2007)
-early_means <- as.numeric(early_period$mean)
-late_means <- as.numeric(late_period$mean)
-print(Malaria_data_Vietnam)
+  pull("mean")
 
-# Perform an independent t-test
-Malaria_t_test <- t.test(early_means, late_means, alternative = "two.sided", conf.level = 0.95)
-print(Malaria_t_test)
+Malaria_data_Vietnam <- as.numeric(Malaria_data_Vietnam)
 
-# Results: Two Sample t-test, t-value = 5.5193, p-value = 0.0003262
-# alternative hypothesis: true mean difference is not equal to 0. Since the p-value < 0.05, we reject null hypothesis. Therefore, Malaria deaths have decreased in Vietnam between the years 2002 and 2013.
-# Visualization
-HIV_data_Vietnam$mean <- as.numeric(HIV_data_Vietnam$mean)
-TB_data_Vietnam$mean <- as.numeric(TB_data_Vietnam$mean)
-Malaria_data_Vietnam$mean <- as.numeric(Malaria_data_Vietnam$mean)
+# ===================================================================
 
-HIV_data_Vietnam_sum <- sum(HIV_data_Vietnam, na.rm = TRUE)
-TB_data_Vietnam_sum <- sum(TB_data_Vietnam, na.rm = TRUE)
-Malaria_data_Vietnam_sum <- sum(Malaria_data_Vietnam, na.rm = TRUE)
-
-summary_data <- data.frame(
-  condition = rep(c("HIV", "Malaria", "TB"), each = 2),
-  year = rep(c(2002, 2013), times = 3),
-  sum_value = c(HIV_data_Vietnam_sum, TB_data_Vietnam_sum, Malaria_data_Vietnam_sum)
+HIV_t_test <- t.test(
+  x = HIV_data_Somalia,
+  y = HIV_data_Vietnam,
+  alternative = "two.sided", 
+  paired = FALSE,
+  conf.level = 0.95
 )
 
-print(summary_data)
+HIV_t_test
 
-ggplot(summary_data, aes(x = condition, y = sum_value, fill = factor(year))) +
-  geom_bar(stat = "identity", position = "dodge") +
-  scale_fill_manual(values = c("2002" = "cornflowerblue", "2013" = "darkslategray1")) +
+TB_t_test <- t.test(
+  x = TB_data_Somalia,
+  y = TB_data_Vietnam,
+  alternative = "two.sided", 
+  paired = FALSE,
+  conf.level = 0.95
+)
+
+TB_t_test
+
+Malaria_t_test <- t.test(
+  x = Malaria_data_Somalia,
+  y = Malaria_data_Vietnam,
+  alternative = "two.sided", 
+  paired = FALSE,
+  conf.level = 0.95
+)
+
+Malaria_t_test
+
+# =================================================================
+
+# Combine HIV, TB, and Malaria data into a tidy dataframe
+years <- 2002:2013
+
+# Create dataframes for each disease and country
+HIV_Somalia <- data.frame(
+  year = years,
+  location = "Somalia",
+  cause = "HIV/AIDS",
+  rate = HIV_data_Somalia
+)
+
+HIV_Vietnam <- data.frame(
+  year = years,
+  location = "Vietnam",
+  cause = "HIV/AIDS",
+  rate = HIV_data_Vietnam
+)
+
+TB_Somalia <- data.frame(
+  year = years,
+  location = "Somalia",
+  cause = "Tuberculosis",
+  rate = TB_data_Somalia
+)
+
+TB_Vietnam <- data.frame(
+  year = years,
+  location = "Vietnam",
+  cause = "Tuberculosis",
+  rate = TB_data_Vietnam
+)
+
+Malaria_Somalia <- data.frame(
+  year = years,
+  location = "Somalia",
+  cause = "Malaria",
+  rate = Malaria_data_Somalia
+)
+
+Malaria_Vietnam <- data.frame(
+  year = years,
+  location = "Vietnam",
+  cause = "Malaria",
+  rate = Malaria_data_Vietnam
+)
+
+# Combine all dataframes into one
+mortality_data <- bind_rows(HIV_Somalia, HIV_Vietnam, TB_Somalia, TB_Vietnam, Malaria_Somalia, Malaria_Vietnam)
+
+# ===========================================================================
+
+ggplot(mortality_data, aes(x = year, y = rate, color = location, linetype = cause)) +
+  geom_line(size = 1) + 
+  geom_point(size = 2) + 
   labs(
-    title = "Sum of Deaths for HIV, Malaria, and TB in Vietnam (2002 vs. 2013)",
-    x = "Disease",
-    y = "Sum of Deaths",
-    fill = "Year"
+    title = "Mortality Rates of HIV, TB, and Malaria in Somalia and Vietnam (2002–2013)",
+    x = "Year",
+    y = "Mortality Rate",
+    color = "Country",
+    linetype = "Cause of Death"
   ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    legend.text = element_text(size = 12)
+  )
